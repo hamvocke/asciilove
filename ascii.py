ea0ae386af18fbaac3ec77e26a54b6409c0488e4
Ham Vocke <hermann.vocke@gmail.com>
2015-09-06 10:40:20 +0200

    writing to stdout, reading image path as first parameter

--- ascii.py
+++ ascii.py
@@ -1,5 +1,11 @@
-from wand.image import Image, COLORSPACE_TYPES
-from wand.display import display
+#!/usr/bin/env python
+
+from wand.image import Image
+import argparse
+
+parser = argparse.ArgumentParser(description='Turn an image into ascii art')
+parser.add_argument('image', help='image path')
+args = parser.parse_args()
 
 symbols = {0.8: " ", 0.6: "░", 0.4:"▒", 0.2: "▓", 0.0: "█"}
 
@@ -9,16 +15,16 @@ def char_for_color(color):
     for key in symbols:
         if color.red >= key:
             return symbols[key]
-    return symbols[0.6]
+    return symbols[0.8]
 
-with Image(filename='ham.jpg') as img:
+with Image(filename=args.image) as img:
     img.modulate(saturation=0)
-    img.level(black=0.2, white=0.8)
+    #img.level(black=0.2, white=0.8)
+    img.negate()
     img.resize(height=img.height//3, width=img.width//3)
     for row in img:
         for col in row:
             output += char_for_color(col)
         output += "\n"
 
-with open('output.txt', 'w') as f:
-    f.write(output)
+print(output)
