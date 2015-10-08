a7fc5d5ebc204b465155c19c769931c7fa8cb735
Hermann Vocke <hvocke@thoughtworks.com>
2015-10-08 14:03:49 +0200

    some more design stuff, first javascript

--- ascii.py
+++ ascii.py
@@ -18,7 +18,7 @@ def char_for_color(color):
 with Image(filename=args.image) as img:
     img.modulate(saturation=0)
     #img.level(black=0.2, white=0.8)
-    img.negate()
+    #img.negate()
     img.resize(height=int(img.height//5), width=img.width//3)
     for row in img:
         for col in row:
