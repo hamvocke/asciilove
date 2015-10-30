61ef044e3265ea9f08bfc361dd730ba19646850a
Hermann Vocke <hvocke@thoughtworks.com>
2015-10-30 09:39:10 +0100

    python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='asciilove',
+	version='0.1',
+	description='Convert images to ASCII art in your command line',
+	url='https://github.com/hamvocke/asciilove',
+	author='Hermann Vocke',
+	author_email='ham@hamvocke.com',
+	license='MIT',
+	package=['asciilove'],
+	zip_safe=False)
