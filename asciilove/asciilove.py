6f5ea2132a7fe0e7721df3dc761ac5511d761f22
Hermann Vocke <hvocke@thoughtworks.com>
2015-11-02 09:47:36 +0100

    added first tests

--- asciilove/asciilove.py
+++ asciilove/asciilove.py
@@ -4,7 +4,6 @@ from wand.image import Image
 
 symbols = {1.0: " ", 0.9: " ", 0.8: "░",  0.7: "░", 0.6: "▒", 0.5: "▒", 0.4:"▒", 0.3: "▓", 0.2: "▓", 0.1: "█", 0.0: "█"}
 
-
 def char_for_color(color):
     return symbols[round(color.red, 1)]
 
---
+++ asciilove/tests/test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from asciilove import asciilove
+
+class Color():
+    red = 0.0
+    green = 0.0
+    blue = 0.0
+
+    def __init__(self, red, green, blue):
+	self.red = red
+	self.green = green
+	self.blue = blue
+
+class TestJoke(TestCase):
+
+    def test_char_for_color_should_return_blank_character(self):
+	self.assertEqual(asciilove.char_for_color(Color(1.0, 0.0, 0.0)), " ")
+	self.assertEqual(asciilove.char_for_color(Color(0.99, 1.0, 0.7)), " ")
+	self.assertEqual(asciilove.char_for_color(Color(0.95, 0.9, 0.2)), " ")
+	self.assertEqual(asciilove.char_for_color(Color(0.90, 0.2, 0.4)), " ")
