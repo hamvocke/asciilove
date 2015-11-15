4c36c6808dfd64f991fa48c5d6c1a537a169a75a
Ham Vocke <hermann.vocke@gmail.com>
2015-11-15 10:57:03 +0100

    fixing tabs/spaces inconsistencies

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(name='asciilove',
-	version='0.1',
-	description='Convert images to ASCII art in your command line',
-	url='https://github.com/hamvocke/asciilove',
-	author='Hermann Vocke',
-	author_email='ham@hamvocke.com',
-	license='MIT',
-	packages=['asciilove'],
-	install_requires=[
-	    'wand'
-	],
-	entry_points={
-	    'console_scripts': ['asciilove=asciilove.commandline:main'],    
-	}
-	)
+        version='0.1',
+        description='Convert images to ASCII art in your command line',
+        url='https://github.com/hamvocke/asciilove',
+        author='Hermann Vocke',
+        author_email='ham@hamvocke.com',
+        license='MIT',
+        packages=['asciilove'],
+        install_requires=[
+            'wand'
+        ],
+        entry_points={
+            'console_scripts': ['asciilove=asciilove.commandline:main'],    
+        }
+    )
