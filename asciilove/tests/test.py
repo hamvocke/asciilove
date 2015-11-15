38191eb606380aa17dc8b10a940bc3b09e7d0b84
Ham Vocke <hermann.vocke@gmail.com>
2015-11-15 11:13:56 +0100

    more tests

--- asciilove/tests/test.py
+++ asciilove/tests/test.py
@@ -16,5 +16,12 @@ class ColorCharTest(TestCase):
     def test_char_for_color_should_return_blank_character(self):
         self.assertEqual(asciilove.char_for_color(Color(1.0, 0.0, 0.0)), " ")
         self.assertEqual(asciilove.char_for_color(Color(0.99, 1.0, 0.7)), " ")
-        self.assertEqual(asciilove.char_for_color(Color(0.95, 0.9, 0.2)), " ")
-        self.assertEqual(asciilove.char_for_color(Color(0.90, 0.2, 0.4)), " ")
+        self.assertEqual(asciilove.char_for_color(Color(0.89, 0.9, 0.2)), " ")
+        self.assertEqual(asciilove.char_for_color(Color(0.86, 0.2, 0.4)), " ")
+
+    def test_should_return_shade_one(self):
+        self.assertEqual(asciilove.char_for_color(Color(0.80, 0.0, 0.0)), "░")
+        self.assertEqual(asciilove.char_for_color(Color(0.66, 1.0, 0.0)), "░")
+        self.assertEqual(asciilove.char_for_color(Color(0.799, 0.0, 0.95)), "░")
+        self.assertEqual(asciilove.char_for_color(Color(0.65, 0.4, 0.6)), "░")
+
